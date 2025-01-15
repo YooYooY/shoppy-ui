0b22bbe59bf521edba3a7db0242490ca19b4a490
chenwl <13751840799@163.com>
2025-01-15 11:07:03 +0800

    feat: Create Product Server Action

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,10 +1,10 @@
 'use server'
 
-import { FormError } from '@/app/common/interfaces/form-error.interface'
+import { FormResponse } from '@/app/common/interfaces/form-response.interface'
 import { post } from '@/app/common/util/fetch'
 import { redirect } from 'next/navigation'
 
-export default async function createUser(_prevState: FormError, formData: FormData) {
+export default async function createUser(_prevState: FormResponse, formData: FormData) {
   const { error } = await post('users', formData)
 
   if (error) {
